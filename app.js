b50c8aadd114138639d66f3f14d8b8e3a2196038
Sami Onk <112951587+Onksami@users.noreply.github.com>
2023-10-23 21:21:51 +0200

    app.js

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ let nextBtn = document.getElementById ("nextBtn");
 scrollContainer.addEventListener ( "wheel", (evt) => {
     evt.preventDefault();
     scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainer.style.scrollBehavior = "auto";
 
 });
 nextBtn.addEventListener("click", () => {
     scrollContainer.scrollLeft += 900;
+    scrollContainer.style.scrollBehavior = "smooth";
 });
 
 backBtn.addEventListener("click", () => {
     scrollContainer.scrollLeft -= 900;
+    scrollContainer.style.scrollBehavior = "smooth";
 });
